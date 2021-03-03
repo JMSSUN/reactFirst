b8c2ba5cc9b56ef847a2df7dc2e678843ee21869
jmssun <j_mssun@naver.com>
2021-03-04 01:01:25 +0900

    input

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, {Component} from 'react';
 import MyName from './MyName';
 import Counter from './Counter';
+import PhoneForm from './components/PhoneForm';
 
 class App extends Component {
+  handleCreate = (data) => {
+    console.log(data);
+  }
   render() {
     return (
       // <MyName name="리액트" />
-      <Counter />
+      // <Counter />
+      <PhoneForm onCreate={this.handleCreate}/>
     );
   }
 }
